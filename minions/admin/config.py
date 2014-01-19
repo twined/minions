4bc91e7d170b88025132477eea2d2b1be7c51e25
Trond Mjøen <trond@twined.net>
2014-01-20 00:44:29 +0100

    - updating menu defs for tw 2.0

--- minions/admin/config.py
+++ minions/admin/config.py
@@ -5,15 +5,21 @@ APP_ADMIN_URLS = {
 }
 APP_ADMIN_MENU = {
     'Brukere': {
-        'Oversikt': {
-            'url': reverse_lazy('admin:users:list'),
-            'icon': 'icon-th-list',
-            'order': 0,
-        },
-        'Legg til': {
-            'url': reverse_lazy('admin:users:create'),
-            'icon': 'icon-plus-sign',
-            'order': 1,
-        },
+        'anchor': 'users',
+        'bgcolor': '#FFC333',
+        'icon': 'fa fa-user icon',
+
+        'menu': {
+            'Oversikt': {
+                'url': reverse_lazy('admin:users:list'),
+                'icon': 'icon-th-list',
+                'order': 0,
+            },
+            'Legg til': {
+                'url': reverse_lazy('admin:users:create'),
+                'icon': 'icon-plus-sign',
+                'order': 1,
+            },
+        }
     }
 }
