5d9b14bdb3f79351b396eaaf982e7f6c03b2d089
Trond Mjøen <trond@twined.net>
2014-11-02 20:40:26 +0100

    - cleanup

--- minions/admin/urls.py
+++ minions/admin/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, url
 from minions.admin.views import (
-    CreateUsersView, ListUsersView,
-    DeleteUsersView, UpdateUsersView,
-    ChangePasswordUsersView,
-    UpdateProfileView)
+    UsersCreateView, UsersListView,
+    UsersDeleteView, UsersUpdateView,
+    PasswordChangeView,
+    ProfileUpdateView)
 
 urlpatterns = patterns(
     '',
-    url(r'^profil/$', UpdateProfileView.as_view(), name="profile"),
-    url(r'^oversikt/$', ListUsersView.as_view(), name="list"),
-    url(r'^ny/$', CreateUsersView.as_view(), name="create"),
-    url(r'^slett/(?P<pk>\d+)/$', DeleteUsersView.as_view(), name="delete"),
-    url(r'^endre/(?P<pk>\d+)/$', UpdateUsersView.as_view(), name="update"),
-    url(r'^endre/(?P<pk>\d+)/password', ChangePasswordUsersView.as_view(),
+    url(r'^profil/$', ProfileUpdateView.as_view(), name="profile"),
+    url(r'^oversikt/$', UsersListView.as_view(), name="list"),
+    url(r'^ny/$', UsersCreateView.as_view(), name="create"),
+    url(r'^slett/(?P<pk>\d+)/$', UsersDeleteView.as_view(), name="delete"),
+    url(r'^endre/(?P<pk>\d+)/$', UsersUpdateView.as_view(), name="update"),
+    url(r'^endre/(?P<pk>\d+)/password', PasswordChangeView.as_view(),
         name="password"),
 )
--- minions/admin/views.py
+++ minions/admin/views.py
@@ -1,25 +1,34 @@
-# posts.py
+# -*- coding: utf-8 -*-
+"""
+// twined - minions
+// admin views definitions for the minions app
+// (c) Twined/Univers 2009-2014. All rights reserved.
+"""
 
 from django.contrib import messages
-from django.core.urlresolvers import reverse_lazy
-from django.views.generic import (CreateView, ListView,
-                                  UpdateView, DeleteView, FormView)
-
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.views.generic import FormView
+from django.views.generic import ListView
 
-from minions.forms import (UserForm, UserCreateForm, PassChangeForm,
-                           UserProfileForm)
+from minions.forms import PassChangeForm
+from minions.forms import UserCreateForm
+from minions.forms import UserForm
+from minions.forms import UserProfileForm
 from minions.models import UserProfile
 from minions.views import LoginRequiredMixin
 
 
-class ListUsersView(LoginRequiredMixin, ListView):
+class UsersListView(LoginRequiredMixin, ListView):
     model = User
     context_object_name = "users"
     template_name = "users/admin/list.html"
 
 
-class CreateUsersView(LoginRequiredMixin, CreateView):
+class UsersCreateView(LoginRequiredMixin, CreateView):
     form_class = UserCreateForm
     template_name = "users/admin/create.html"
     success_url = reverse_lazy('admin:users:list')
@@ -28,14 +37,14 @@ class CreateUsersView(LoginRequiredMixin, CreateView):
         self.object = form.save(commit=False)
         self.object.user = self.request.user
         messages.success(self.request, "Success", extra_tags='msg')
-        return super(CreateUsersView, self).form_valid(form)
+        return super(UsersCreateView, self).form_valid(form)
 
     def form_invalid(self, form):
         messages.error(self.request, "Rett feilene under")
-        return super(CreateUsersView, self).form_invalid(form)
+        return super(UsersCreateView, self).form_invalid(form)
 
 
-class UpdateUsersView(LoginRequiredMixin, UpdateView):
+class UsersUpdateView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserForm
     template_name = "users/admin/update.html"
@@ -47,14 +56,14 @@ class UpdateUsersView(LoginRequiredMixin, UpdateView):
         self.object.user = self.request.user
         messages.success(self.request, "Endringen var vellykket.",
                          extra_tags='msg')
-        return super(UpdateUsersView, self).form_valid(form)
+        return super(UsersUpdateView, self).form_valid(form)
 
     def form_invalid(self, form):
         messages.error(self.request, "Rett feilene under")
-        return super(UpdateUsersView, self).form_invalid(form)
+        return super(UsersUpdateView, self).form_invalid(form)
 
 
-class DeleteUsersView(LoginRequiredMixin, DeleteView):
+class UsersDeleteView(LoginRequiredMixin, DeleteView):
     model = User
     template_name = "users/admin/user_confirm_delete.html"
     success_url = reverse_lazy('admin:users:list')
@@ -64,14 +73,14 @@ class DeleteUsersView(LoginRequiredMixin, DeleteView):
         # set the owner to be the current user
         messages.success(self.request, "Endringen var vellykket.",
                          extra_tags='msg')
-        return super(UpdateUsersView, self).form_valid(form)
+        return super(UsersUpdateView, self).form_valid(form)
 
     def form_invalid(self, form):
         messages.error(self.request, "Rett feilene under")
-        return super(UpdateUsersView, self).form_invalid(form)
+        return super(UsersUpdateView, self).form_invalid(form)
 
 
-class UpdateProfileView(LoginRequiredMixin, UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = UserProfile
     form_class = UserProfileForm
     template_name = "users/admin/profile.html"
@@ -89,20 +98,20 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
         self.object.user = self.request.user
         messages.success(self.request, "Endringen var vellykket.",
                          extra_tags='msg')
-        return super(UpdateProfileView, self).form_valid(form)
+        return super(ProfileUpdateView, self).form_valid(form)
 
     def form_invalid(self, form):
         messages.error(self.request, "Rett feilene under")
-        return super(UpdateProfileView, self).form_invalid(form)
+        return super(ProfileUpdateView, self).form_invalid(form)
 
 
-class ChangePasswordUsersView(LoginRequiredMixin, FormView):
+class PasswordChangeView(LoginRequiredMixin, FormView):
     form_class = PassChangeForm
     template_name = "users/admin/change_password.html"
     success_url = reverse_lazy('admin:users:list')
 
     def get_form_kwargs(self):
-        kwargs = super(ChangePasswordUsersView, self).get_form_kwargs()
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
 
@@ -111,4 +120,4 @@ class ChangePasswordUsersView(LoginRequiredMixin, FormView):
         messages.success(self.request, "Endringen var vellykket.",
                          extra_tags='msg')
         form.save()
-        return super(ChangePasswordUsersView, self).form_valid(form)
+        return super(PasswordChangeView, self).form_valid(form)
